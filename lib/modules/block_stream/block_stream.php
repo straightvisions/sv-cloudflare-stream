92eb5062c5a26fa8175a4ce25f9da649b05daa10
Dennis Heiden <dennis.heiden@straightvisions.com>
2022-05-09 16:47:34 +0200

    add working editor block output

--- lib/modules/block_stream/block_stream.php
+++ lib/modules/block_stream/block_stream.php
@@ -5,11 +5,11 @@
 		public function init() {
 			
 			$this->load_settings()
-			     ->register_scripts()
-			     ->register_block();
+			     ->register_scripts();
 			
 			add_shortcode('sv_cloudflare_stream', array($this, 'shortcode'));
-			
+			//add_action( 'init', array($this, 'register_block' ));
+			add_action( 'init', array($this, 'register_block') );
 		}
 		
 		protected function load_settings(): block_stream {
@@ -17,10 +17,29 @@
 		}
 		
 		public function register_scripts(): block_stream {
+			
+			$this->get_script('sv-cloudflare-stream-editor-script')
+			     ->set_path('lib/backend/dist/block.build.js')
+			     ->set_type('js')
+				->set_is_gutenberg()
+			     ->set_is_backend()
+				->set_deps(array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'))
+				 ->set_is_enqueued();
+			
 			return $this;
 		}
 		
 		public function register_block(): block_stream{
+			$path = $this->get_path('block.json') ;
+			
+			if ( is_string( $path ) && file_exists( $path ) ) {
+				register_block_type_from_metadata( $path );
+			}else{
+				WP_Block_Type_Registry::get_instance()->register( $path );
+			}
+			
+			
+			
 			return $this;
 		}
 		
