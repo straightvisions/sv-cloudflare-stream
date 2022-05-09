1eea8e6a62b117a9ecc5cbc6c2cd634a19d817f6
Dennis Heiden <dennis.heiden@straightvisions.com>
2022-05-09 14:13:32 +0200

    fix plugin activation fatal errors after refactoring

--- lib/modules/block_stream/block_stream.php
+++ lib/modules/block_stream/block_stream.php
@@ -1,21 +1,13 @@
 <?php
 	namespace sv_cloudflare_stream;
 
-	class iframe extends modules {
+	class block_stream extends modules {
 		public function init() {
-			add_shortcode('sv_iframe', array($this, 'shortcode'));
+			add_shortcode('sv_cloudflare_stream', array($this, 'shortcode'));
 		}
 		public function shortcode($atts, $content = null){
-			$atts = shortcode_atts(
-				array(
-					'width'          	=> '100%',
-					'height'			=> '700px',
-					'url'				=> ''
-				),
-				$atts,
-				$this->get_prefix()
-			);
+			
 
-			return '<iframe src="'.$atts['url'].'" frameborder="0" width="'.$atts['width'].'" height="'.$atts['height'].'"></iframe>';
+			return '';
 		}
 	}
--- sv_cloudflare_stream.php
+++ sv_cloudflare_stream.php
@@ -1,4 +1,16 @@
 <?php
+/*
+Version: 1.8.00
+Plugin Name: SV Cloudflare Stream
+Text Domain: sv_cloudflare_stream
+Description: Adds a Gutenberg video block to add Cloudflare streamed videos
+Plugin URI: https://straightvisions.com/
+Author: straightvisions GmbH, Cloudflare, WP Engine
+Author URI: https://straightvisions.com
+Domain Path: /languages
+License: GPL-3.0-or-later
+License URI: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+*/
 namespace sv_cloudflare_stream;
 
 if(!class_exists('\sv_dependencies\init')){
